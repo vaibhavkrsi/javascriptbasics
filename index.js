794b5007d2b2bdb34dfbe762ff72c7a9e6897faa
vaibhavkrsi <23090282-vaibhavkrsi@users.noreply.replit.com>
2023-05-14 20:34:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let arr = [1,2,3,4];
+let newarr = arr.reduce((h1,h2)=>{
+  return h1+h2;
+}) 
+console.log(newarr);  
+
+// let arr = [1,2,3,4];
+// let newarr = arr.filter((u)=>{
+//   return u>3;
+// } )
+// console.log(newarr);
+
+
+// let arr = [1,2,3,4];
+// let newarr = arr.map((value,index)=>{
+//    console.log(value,index);
+// }) 
+// console.log(arr)
+
+// let counter = countCo();
+// function countCo(){
+//   return {
+//     count:0,
+//     inc:function(){
+//       this.count++;
+//     }
+//   }
+// } 
+// counter.inc();
+// counter.inc();
+// console.log(counter)
